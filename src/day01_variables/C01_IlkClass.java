bbfc8934153031234b36ac3f13b29a639716f9c7
tatis35 <94759092+tatis35@users.noreply.github.com>
2022-03-25 22:11:54 +0100

    Update C01_IlkClass.java

--- src/day01_variables/C01_IlkClass.java
+++ src/day01_variables/C01_IlkClass.java
@@ -4,6 +4,9 @@ public class C01_IlkClass {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        
+        // Bunu github icin yaziyorum.
 
     }
 }
